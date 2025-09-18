756467118bd6a012d097fbc68329888b4319f27f
whikseys <belysh8@mail.ru>
2025-09-18 22:19:56 +0300

    Corrected MainWindow::MainWindow for Windows --> develop

--- src/MainWindow/mainwindow.cpp
+++ src/MainWindow/mainwindow.cpp
@@ -6,8 +6,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
   ui->setupUi(this);
 
   ui->spinBoxTimer->setEnabled(ui->checkBoxTimer->isChecked());
-  //connect(ui->checkBoxTimer, &QCheckBox::checkStateChanged, this, &MainWindow::checkBoxChanged);
-  connect(ui->checkBoxTimer, &QCheckBox::stateChanged, this, &MainWindow::checkBoxChanged);
+  connect(ui->checkBoxTimer, &QCheckBox::checkStateChanged, this, &MainWindow::checkBoxChanged);//Windows
+  //connect(ui->checkBoxTimer, &QCheckBox::stateChanged, this, &MainWindow::checkBoxChanged);//Linux
   connect(ui->oneTimeLaunchButton, &QPushButton::clicked, this, &MainWindow::workOneTineLaunch);
   connect(ui->cycleWorkButton, &QPushButton::clicked, this, &MainWindow::cycleWork);
 }
